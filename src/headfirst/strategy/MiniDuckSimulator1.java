e2f5d1e41a1fe2c0606d6b5391320a210ef3a920
201415059 _장민섭 <jms0995@hanmail.net>
2017-03-28 09:22:47 +0900

    Update MiniDuckSimulator1.java

--- src/headfirst/strategy/MiniDuckSimulator1.java
+++ src/headfirst/strategy/MiniDuckSimulator1.java
@@ -1,9 +1,13 @@
 package headfirst.strategy;
 
 public class MiniDuckSimulator1 {
- 
- public static void main(String[] args) {
-  Duck mallard = new MallardDuck();
-  mallard.performFly();
- }
+  public static void main(String[] args) {
+    Duck mallard = new MallardDuck();
+    mallard.performQuack();
+    mallard.performFly();
+    Duck model = new ModelDuck();
+    model.performFly();
+    model.setFlyBehavior(new FlyRocketPowered());
+    model.performFly();
+  }
 }
