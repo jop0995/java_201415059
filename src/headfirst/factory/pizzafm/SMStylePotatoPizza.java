ec996ce17b8b342edb720bc01383e028709609db
201415059 _장민섭 <jms0995@hanmail.net>
2017-04-19 15:35:56 +0900

    Add files via upload

---
+++ src/headfirst/factory/pizzafm/SMStylePotatoPizza.java
@@ -0,0 +1,12 @@
+package headfirst.factory.pizzafm;
+
+public class SMStylePotatoPizza extends Pizza {
+  public SMStylePotatoPizza() {
+  name = "SM Style Sauce and Potato Pizza";
+  dough = "Thin Crust Dough";
+  sauce = "Marinara souce";
+  toppings.add("Potato");
+  toppings.add("Garlic");
+  toppings.add("Mushrooms");
+ }
+}
